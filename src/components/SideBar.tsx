c1bd7656a3568317a978e729eb105543f40f0bb6
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2021-09-19 22:25:04 -0300

    creating sidebar and content component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,40 @@
-export function SideBar() {
-  // Complete aqui
+import '../styles/sidebar.scss';
+
+import { Button } from './Button';
+
+interface Genre {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SidebarProps {
+  genres: Genre[];
+  buttonClickCallBack: (id: number) => void;
+  selectedGenreId: number;
+}
+export function SideBar({
+  genres,
+  buttonClickCallBack,
+  selectedGenreId,
+}: SidebarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => buttonClickCallBack(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
